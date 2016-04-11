04c0e6780fa0c760d7244efbabf813026c5df956
yhao <yhao@ideabinder.com>
2016-04-11 11:03:41 +0800

    更gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,19 @@ var less = require("gulp-less");
 var minify = require("gulp-minify-css");
 var autoPrefix = require("gulp-autoPrefixer");
 var babel = require ("gulp-babel");
+var source = require("vinyl-source-stream");
 
 var path = require("path");
 var rename = require("gulp-rename");
+var assign = require("lodash.assign");
+
 
 
 var port = process.env.port||5000;
 
 
 var src = {
+	index:"./src/index.html",
 	js:"./src/scripts",
 	css:"./src/styles",
 	html:"./src/views",
@@ -24,48 +28,39 @@ var src = {
 }
 
 var dist = {
+	index:"./dist",
 	js:"./dist/js",
 	css:"./dist/css",
 	html:"./dist/view",
 	lib:"./dist/lib"
 }
 
+// var opts = assign({},watchify.args,{ 
+// 		debug:true,
+// 		transform:["reactify"]
+// 	});
 
-//模块化
-gulp.task("browserify",function(){
-	return gulp.src(src.js+"/**/*.js")
-		.pipe(watchify(browserify(
-			{
-				transform:["reactify"]
-			}
-	)))
-	.pipe(uglify())
-	.pipe(gulp.dest("./dist/js/"));
+// var b = watchify(browserify(opts));  //创建browserify实例 
+// b.on("update",bundle);//当依赖发生变化时，运行打包工具
 
-	gulp.src(src.css+"/**/*.less")
-	.pipe(less())
-	.pipe(gulp.dest("./dist/css/"));
+// function bundle(){
+// 	return b.bundle();
+// 		.pipe()
+// }
 
-	gulp.src(src.html+"/**/*.html")
-	.pipe(gulp.dest("./dist/views/"));
 
-	gulp.src("./src/index.html")
-	.pipe(gulp.dest("./dist/"));
-
-	gulp.src(src.lib+"/**/*.css")
-	.pipe(gulp.dest("./dist/lib/"))
-});
 
 //jsx
 gulp.task("jsx",function(){
 	return gulp.src(path.join(__dirname,src.js,"/**/*.js"))
 		//.pipe(babel({presets:["es2015"]}))
-		.pipe(react())
-		// .pipe(browserify(
-		// 	{
-		// 		transform:["reactify"]
-		// 	}
-		// ))
+		//.pipe(browserify())
+		//.pipe(react())
+		.pipe(browserify(
+			{
+				transform:["reactify"]
+			}
+		))
 		.pipe(uglify())
 		.pipe(gulp.dest(path.join(__dirname,dist.js)))
 });
@@ -80,15 +75,18 @@ gulp.task("less",function(){
 		.pipe(gulp.dest(path.join(__dirname,dist.css)))
 });
 
+
 //html
-gulp.task("tpl",function(){
-	return gulp.src(
-		[
-			path.join(__dirname,"./src/**/*.html")
-		])
-		.pipe(gulp.dest(path.join(__dirname,"./dist/")))
+gulp.task("tpl",["index"],function(){
+	return gulp.src(path.join(__dirname,src.html,"/**/*.html"))
+		.pipe(gulp.dest(path.join(__dirname,dist.html)))
 });
 
+//index.html
+gulp.task("index",function(){
+	return gulp.src(path.join(__dirname,src.index))
+		.pipe(gulp.dest(path.join(__dirname,dist.index)))
+});
 
 
 //web server
@@ -104,22 +102,19 @@ gulp.task("connect",function(){
 //js
 gulp.task("js",function(){
 	return gulp.src(path.join(__dirname,dist.js,"/**/*.js"))
-	.pipe(connect.reload())
+		.pipe(connect.reload())
 });
 
 //css
 gulp.task("css",function(){
 	return gulp.src(path.join(__dirname,dist.css,"/**/*.css"))
-	.pipe(connect.reload())
+		.pipe(connect.reload())
 });
 
 //html
 gulp.task("html",function(){
-	return gulp.src(
-		[
-			path.join(__dirname,"./dist/**/*.html")
-		])
-	.pipe(connect.reload());
+	return gulp.src(path.join(__dirname,"./dist/**/*.html"))
+		.pipe(connect.reload());
 });
 
 
@@ -137,25 +132,17 @@ gulp.task("antd",function(){
 });
 
 
-
-
 gulp.task("watch",function(){
 	var watchArr = [];
 
 	watchArr.push(gulp.watch(path.join(__dirname,src.js,"/**/*.js"),["jsx"]));
 	watchArr.push(gulp.watch(path.join(__dirname,src.css,"/**/*.less"),["less"]));
-	watchArr.push(gulp.watch(
-		[
-			path.join(__dirname,"./src/**/*.html")
-		],["tpl"]));
+	watchArr.push(gulp.watch(path.join(__dirname,src.html,"/**/*.html"),["tpl"]));
+	watchArr.push(gulp.watch(path.join(__dirname,src.index),["index"]));
 
 	watchArr.push(gulp.watch(path.join(__dirname,dist.js,"/**/*.js"),["js"]));
 	watchArr.push(gulp.watch(path.join(__dirname,dist.css,"/**/*.css"),["css"]));
-	watchArr.push(gulp.watch(
-		[
-			path.join(__dirname,"./dist/**/*.html")
-
-		],["html"]));
+	watchArr.push(gulp.watch(path.join(__dirname,"./dist/**/*.html"),["html","index"]));
 
 	watchArr.forEach(function(watch,i){
 		watch.on("change",function(e){
@@ -164,6 +151,9 @@ gulp.task("watch",function(){
 	});
 	
 });
+
+
+//可执行任务
 
 gulp.task("lib",["antd"]);
 
